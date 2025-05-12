e5fca6e521477dce24649f02f77115339c25515c
Nicolas Biff <biffnicolas7@gmail.com>
2025-05-12 20:38:39 -0300

    Atualização dos códigos do trabalho novamente

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',        
-  password: '',        
-  database: 'lavajato' 
+  password: 'BiffNick123@',        
+  database: 'lava_rapido' 
 });
 
 connection.connect((err) => {
